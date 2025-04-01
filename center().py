2b9c3f0e0f6d66912b6566052e04e674ab9968f1
dagimbogale <dagibena@gmail.com>
2025-04-01 13:31:49 +0300

    count().py
    Return the number of times the value  appears in the string:

---
+++ center().py
@@ -0,0 +1,4 @@
+txt = "dagim"
+x = txt.center(30)
+print(x)
+#output:                              dagim
---
+++ count().py
@@ -0,0 +1,3 @@
+txt = "apple apple banana mango banana apple mango"
+x = txt.count("apple")
+print(x) #output: 3
